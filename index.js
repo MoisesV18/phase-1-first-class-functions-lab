1b728bcb2c9ffd119f0965ffa6943f1ee437e865
Moises Victoria <moisesvc1815@gmail.com>
2022-12-04 15:00:38 -0800

    commit tree 75df26ecabda88fd82661777badd216188908730

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+const returnFirstTwoDrivers = function(drivers) {
+    const drivers1 = new Array(drivers[0],drivers[1])
+    return drivers1
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    const drivers2= new Array(drivers[2],drivers[3])
+    return drivers2
+}
